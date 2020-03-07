b209eb6a5cd7970169a59e9e6db8eaa08bde7dd3
zsxing99 <zsxing99@gmail.com>
2020-03-07 01:10:58 -0800

    -add nav bar

---
+++ src/components/TopNavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from "../assets/images/pubg-logo.png";
+
+export function TopNavBar(props) {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo"/>
+        </header>
+    );
+}
